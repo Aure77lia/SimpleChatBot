68acd7361db91989bf739dc70b6f4f3058dc0244
Aurélia GOMES <aureliagomes00@gmail.com>
2023-07-05 14:04:56 +0200

    simple request

--- src/controller/openaiController.js
+++ src/controller/openaiController.js
@@ -14,7 +14,6 @@ const chatCompletion = async (prompt) => {
             {
                 model: 'gpt-3.5-turbo',
                 messages: [
-                    { "role": "system", "content": "You are a helpful assistant." },
                     { "role": "user", "content": prompt }
                 ]
             }
