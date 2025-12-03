e9a8180040840b7bc5c3088bb5157129571c5bc1
Spesik <aureliagomes00@gmail.com>
2025-12-03 21:34:22 +0100

    instructions added to ia

--- src/controller/groqController.js
+++ src/controller/groqController.js
@@ -10,7 +10,7 @@ const chatCompletion = async (query) => {
         const chatCompletion = await client.chat.completions.create({
             model: process.env.MODEL_LLM,
             messages: [
-                // {role: "system", content: process.env.GROQ_SYSTEM_PROMPT},
+                {role: "system", content: process.env.GROQ_SYSTEM_PROMPT},
                 {role: "user", content: query}],
         });
 
