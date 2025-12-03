d3712bf299a89ea328de9248f6af4481c7634972
Spesik <aureliagomes00@gmail.com>
2025-12-03 19:32:21 +0100

    changing openai controller connection

--- src/controller/openaiController.js
+++ src/controller/openaiController.js
@@ -1,17 +1,25 @@
 require('dotenv').config();
-const { Configuration, OpenAIApi } = require("openai");
+// const { Configuration, OpenAIApi } = require("openai");
+import OpenAI from "openai";
 
-const configuration = new Configuration({
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY
+// });
+// const openai = new OpenAIApi(configuration);
+
+const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
-const openai = new OpenAIApi(configuration);
-
 
 const chatCompletion = async (query) => {
     console.log("start chatCompletion: "+query+" api key: "+configuration.apiKey);
 
     try {
-        const chatCompletion = await openai.createChatCompletion({
+        // const chatCompletion = await openai.createChatCompletion({
+        //     model: process.env.MODEL_LLM,
+        //     messages: [{role: "user", content: query}],
+        // });
+        const chatCompletion = await client.chat.completions.create({
             model: process.env.MODEL_LLM,
             messages: [{role: "user", content: query}],
         });
